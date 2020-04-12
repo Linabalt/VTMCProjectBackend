762161462f3cfae4af17936d6e98ec61b20efe3a
Renaldas Siautilas <wear96@gmail.com>
2020-04-13 01:17:14 +0300

    retrieve task, delete task, update task

--- src/main/java/com/andriusk/project/service/TaskService.java
+++ src/main/java/com/andriusk/project/service/TaskService.java
@@ -5,6 +5,13 @@ import com.andriusk.project.entity.Task;
 import java.util.List;
 
 public interface TaskService {
-    void createTask(Task task, Long projectId);
-    List<Task> getAllTasks(Long projectId);
+	void createTask(Task task, Long projectId);
+
+	List<Task> getAllTasks(Long projectId);
+
+	Task retrieveIndividualTask(Long projectId, Long taskId);
+
+	void deleteTaskByID(Long projectId, Long taskId);
+
+	void editTask(Long projectId, Long taskId, String taskName, String taskDescription);
 }
--- src/main/java/com/andriusk/project/service/TaskServiceImp.java
+++ src/main/java/com/andriusk/project/service/TaskServiceImp.java
@@ -12,23 +12,54 @@ import java.util.List;
 @Service
 public class TaskServiceImp implements TaskService {
 
-    @Autowired
-    private TaskRepository taskRepository;
+	@Autowired
+	private TaskRepository taskRepository;
 
-    @Autowired
-    private ProjectRepository projectRepository;
+	@Autowired
+	private ProjectRepository projectRepository;
 
-    @Override
-    public void createTask(Task task, Long projectId) {
-        Project project = projectRepository.findById(projectId).get();
-        task.setProject(project);
-        taskRepository.save(task);
+	@Override
+	public void createTask(Task task, Long projectId) {
+		Project project = projectRepository.findById(projectId).get();
+		task.setProject(project);
+		taskRepository.save(task);
 //        projectRepository.findById(projectId).get().add(task);
-    }
+	}
 
-    @Override
-    public List<Task> getAllTasks(Long projectId) {
-        return projectRepository.findById(projectId).get().getTasks();
+	@Override
+	public List<Task> getAllTasks(Long projectId) {
+		return projectRepository.findById(projectId).get().getTasks();
 //        return projectRepository.viewAllTasks(projectId);
-    }
+	}
+
+	@Override
+	public Task retrieveIndividualTask(Long projectID, Long taskID) {
+
+		return projectRepository.findById(projectID).get().getTasks().stream().filter(t -> t.getTaskId().equals(taskID))
+				.findFirst().get();
+
+	}
+
+	@Override
+	public void deleteTaskByID(Long projectID, Long taskID) {
+
+		taskRepository.deleteById(taskID);
+
+//		 nelabai supratau kodel situo pagrindu neveikia,
+//		 skamba patikimiau, randa projekto ID pasiema lista,
+//		 ir tada removina. Bet nedirba sitaip.
+//		
+//		 projectRepository.findById(projectID).get().getTasks().remove(retrieveIndividualTask(projectID, taskID));
+
+	}
+
+	@Override
+	public void editTask(Long projectID, Long taskID, String taskName, String taskDescription) {
+
+		Task taskname = retrieveIndividualTask(projectID, taskID);
+		taskname.setTaskDescription(taskDescription);
+		taskname.setTaskName(taskName);
+		taskRepository.save(taskname);
+
+	}
 }
